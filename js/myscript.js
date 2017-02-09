e1c5b0e0d81c8e943bb96fcbe9c71f864cca2dd1
ggkik <ggkik@mail.ru>
2017-02-09 22:49:14 +0200

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,41 +1,85 @@
-window.onload = function(){
-    //time
-    (function(){
+$(document).ready(function(){
+})
+
+    //  Время входа нас траницу(часов, минут, секунд)
+     var dateStart = new Date();
+        var startHours = Math.round(dateStart.getTime()/(1000*60*60));//прошло ч
+        var startMinutes = Math.round(dateStart.getTime()/(1000*60));//прошло м
+        var startSeconds = Math.round(dateStart.getTime()/(1000));//прошло с
+
+    //счетчики времени
+    function dateCount(){
         var date = new Date();
-        li = document.getElementsByTagName('li');
-        var secs = date.getSeconds();
-        var mins = date.getMinutes();
-        var hours = date.getHours();
+        var li = document.getElementsByTagName('li');
+        var secs = date.getSeconds();//сейчас секунд
+        var mins = date.getMinutes();//сейчас минут
+        var hours = date.getHours();//сейчас часов
+
         if(secs<10){
-        	secs="0" + secs;
+            secs="0" + secs;
         }
         if(mins<10){
             mins="0" + mins;
         }
          if(hours<10){
             hours="0" + hours;
-        }
+        };
 
-        li[1].innerHTML = "Your local time is: " + hours+':'+ mins+':'+ "<span style = 'color:kblue'>" + secs +"</span>";
-        //div[1].innerHTML = new Date().setTime(new Date(date).getTime());
-        window.setTimeout(arguments.callee, 1);
-        
-    })();
+        var newHours = Math.round(date.getTime()/(1000*60*60)); //прошло ч обн
+        var newMinutes = Math.round(date.getTime()/(1000*60));//прошло м обн
+        var newSeconds = Math.round(date.getTime()/(1000));//прошло с  обн
+        var hoursDif = newHours - startHours;
+        var minutesDif = newMinutes - startMinutes;
+
+        li[1].innerHTML = "Your local time is: " + hours+':'+ mins+':'+ "<span style = 'color:blue'>" + secs +"</span>";
+
+        li[0].innerHTML = "You are online for: " +"<span style = 'font-weight:700'>"+ hoursDif  + 'h ' + "</span>" + "<span style = 'color:green'>" +minutesDif+'m' + "</span>";
+
+        setInterval(dateCount, 1000);
+    };
+
+    dateCount();
 
         //users online
-        (function(){
+        function countUsersOnline(){
         var usersOnline = $(".usersList>ul>li").length;
         $(".users>p").html("Online: " + usersOnline );
-    })();
+    };
+      countUsersOnline();
+	  
+	  //находим введенный текст
+		var textInputed =  $(".chatinput input").val();
 
-     
-};
+		//send message from input to chat
+		function sendMessage(){
+			var textInputed =  $(".chatinput input").val();
+			var section = $("section[id*=content-tab1]");
+			section.append("<span style = 'color:cornflowerblue'> <b>You</b> : </span>"  + textInputed + '</br>');
+			$("input.form-control").val("");
+		}
 
-function bold(){
-    //находим введенный текст
-  var text =  $(".footer input").val();
-  alert(text);
-  //text.style.font-weight = "700";
-  //text.css('font-weight', '700');
-  
-}
+		//bold
+		function bolding(){
+			$("input.form-control").css("fontWeight", "700");
+		};
+
+		//italic
+		function italic(){
+			$("input.form-control").css("fontStyle", "italic");
+		};
+
+		//uppercase
+		function uppercase(){
+			$("input.form-control").css("textTransform", "uppercase");
+		};
+
+		//link
+		function link(){};
+
+		//add icon before label text on tabs
+		$("label[title*='Вклад']").prepend("<i class='fa fa-user'></i>");
+		$(".fa-user").css({'marginRight' : '6px'});
+
+		//add style overflow-y:auto for scrolling chat list
+		$('section[id*=content]').css({'overflowY':'auto'});
+
